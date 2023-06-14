be9b92c92943cddcb3df2ec6feb6f4e1d72bc3ad
Wu <wu.vinci00@gmail.com>
2023-06-14 16:14:26 -0400

    Removed functions and added outputs to better showcase each round winner and total overal winner

--- main.cpp
+++ main.cpp
@@ -1,70 +1,31 @@
-#include <iostream>
-#include <string>
-
-#define OPTIONS 3
-#define ROCK 0
-#define PAPER 1
-#define SCISSOR 2
-#define PLAYER -1
-#define COMPUTER -2
-#define REDO -3
-
-int computerOption () {
-    return std::rand() % OPTIONS;
-}
-
-std::string roundWinner(int winner) {
-    if (winner == PLAYER) {
-        return "You";
-    }
-    else if (winner == COMPUTER) {
-        return "Computer";
-    }
-    else {
-        return "REDO";
-    }
-}
-
-int declareWinner (int playerOption, int computerOption) {
-    if (playerOption == ROCK) {
-        switch(computerOption) {
-        case PAPER:
-            return COMPUTER;
-        case SCISSOR:
-            return PLAYER;
-        default:
-            return REDO;
-        }
-    }
-    if (playerOption == PAPER) {
-        switch(computerOption) {
-        case ROCK:
-            return COMPUTER;
-        case SCISSOR:
-            return PLAYER;
-        default:
-            return REDO;
-        }
-    }
-    if (playerOption == SCISSOR) {
-        switch(computerOption) {
-        case ROCK:
-            return COMPUTER;
-        case PAPER:
-            return PLAYER;
-        default:
-            return REDO;
-        }
-    }
-    return REDO;
-}
+#include "Game.h"
 
 int main() {
     std::cout << "-----------------------" << std::endl;
     std::cout << "|Rock, Paper, Scissors|" << std::endl;
     std::cout << "-----------------------" << std::endl;
-    for (int i = 0; i < 10; i++) {
-        std::cout << roundWinner(declareWinner(ROCK, computerOption())) << std::endl;
+    int playerChoice, computerChoice, rWinner;
+    int playerPoint = 0, computerPoint = 0;
+    int rounds;
+    std::cout << "How many rounds?" << std::endl;
+    std::cin >> rounds;
+    while (rounds > 0) {
+        computerChoice = computerOption();
+        std::cout << "Choose (0) ROCK, (1) PAPER, (2) SCISSOR: ";
+        std::cin >> playerChoice;
+        std::cout << rpsOptions(playerChoice) << " VS " << rpsOptions(computerChoice) << std::endl;
+        rWinner = declareWinner(playerChoice, computerChoice);
+        std::cout << "Winner: " << roundWinner(rWinner) << "\n" << std::endl;
+        if (rWinner != REDO) {
+            if (rWinner == PLAYER) {
+                playerPoint++;
+            }
+            if (rWinner == COMPUTER) {
+                computerPoint++;
+            }
+            rounds--;
+        }
     }
+    totalWinner(playerPoint, computerPoint);
     return 0;
 }
